df01d9504fad66c11a01f7df2a1590d5711c85e3
Thalles <thalles_leopoldino@outlook.com>
2026-04-19 05:31:54 -0300

    First commit

---
+++ src/main/java/com/bardock/discordvoicebot/DiscordVoiceBotApplication.java
@@ -0,0 +1,13 @@
+package com.bardock.discordvoicebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordVoiceBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscordVoiceBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bardock/discordvoicebot/DiscordVoiceBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bardock.discordvoicebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordVoiceBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
