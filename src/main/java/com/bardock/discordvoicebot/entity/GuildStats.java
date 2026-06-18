111971263a77312ef4caccf544dd25b84b2172ed
Thalles <thalles_leopoldino@outlook.com>
2026-06-18 03:19:45 -0300

    Creating GuildStats entity

---
+++ src/main/java/com/bardock/discordvoicebot/entity/GuildStats.java
@@ -0,0 +1,53 @@
+package com.bardock.discordvoicebot.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.OffsetDateTime;
+
+@Entity
+@Table(name = "guild_stats", uniqueConstraints = {
+        @UniqueConstraint(name = "uq_guild_stats_user_guild", columnNames = {"user_id", "guild_id"})
+})
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class GuildStats {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false, foreignKey = @ForeignKey(name = "fk_guild_stats_user"))
+    private User user;
+
+    @Column(name = "guild_id", nullable = false)
+    private Long guildId;
+
+    @Column(name = "total_time", nullable = false)
+    @Builder.Default
+    private Long totalTime = 0L;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private OffsetDateTime createdAt;
+
+    @Column(name = "created_at", nullable = false)
+    private OffsetDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = OffsetDateTime.now();
+        updatedAt = OffsetDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = OffsetDateTime.now();
+    }
+
+
+}
