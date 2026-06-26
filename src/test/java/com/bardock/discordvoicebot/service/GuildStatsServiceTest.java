7bf4285c19ca1ffdcf1a06cd19907e22ffd93dc8
Thalles <thalles_leopoldino@outlook.com>
2026-06-25 22:25:46 -0300

    addTimeToUser_ExistingRecord implemented

--- src/test/java/com/bardock/discordvoicebot/service/GuildStatsServiceTest.java
+++ src/test/java/com/bardock/discordvoicebot/service/GuildStatsServiceTest.java
@@ -3,6 +3,7 @@ package com.bardock.discordvoicebot.service;
 import com.bardock.discordvoicebot.entity.GuildStats;
 import com.bardock.discordvoicebot.factory.UserTestData;
 import com.bardock.discordvoicebot.repository.GuildStatsRepository;
+import net.dv8tion.jda.api.entities.Guild;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -52,6 +53,31 @@ public class GuildStatsServiceTest {
         assertThat(savedStats.getUser().getId()).isEqualTo(UserTestData.DEFAULT_ID);
     }
 
+    @Test
+    @DisplayName("It should add the time to the user's existing history in the guild")
+    void addTimeToUser_ExistingRecord() {
+        // --- ARRANGE ---
+        Long guildId = 456L;
+        Long newDuration = 200L;
+
+        GuildStats existingStats = new GuildStats();
+        existingStats.setId(1L);
+        existingStats.setGuildId(guildId);
+        existingStats.setTotalTime(1000L);
+
+        when(guildStatsRepository.findByUserIdAndGuildId(UserTestData.DEFAULT_ID, guildId))
+                .thenReturn(Optional.of(existingStats));
+
+        // --- ACTION ---
+        guildStatsService.addTimeToUser(UserTestData.DEFAULT_ID, guildId, newDuration);
+
+        // --- ASSERT ---
+        verify(guildStatsRepository, times(1)).save(statsCaptor.capture());
+
+        GuildStats savedStats = statsCaptor.getValue();
+
+        assertThat(savedStats.getTotalTime()).isEqualTo(1200L);
+    }
 
 
 }
