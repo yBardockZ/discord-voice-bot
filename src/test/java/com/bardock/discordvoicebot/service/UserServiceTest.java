8a4d4e4e13080fc39e6641191bbe0753b0ebac27
Thalles <thalles_leopoldino@outlook.com>
2026-06-29 05:58:23 -0300

    getOrCreateAndUpdateUser_ExistingUser_NoChanges implemented

--- src/test/java/com/bardock/discordvoicebot/service/UserServiceTest.java
+++ src/test/java/com/bardock/discordvoicebot/service/UserServiceTest.java
@@ -27,6 +27,41 @@ public class UserServiceTest {
     private ArgumentCaptor<User> userCaptor;
 
     @Test
+    @DisplayName("Must create and save a new user if one does not exist in the database")
+    void getOrCreateAndUpdateUser_NewUser() {
+        // --- ARRANGE ---
+        when(userRepository.findById(UserTestData.DEFAULT_ID)).thenReturn(Optional.empty());
+        when(userRepository.save(any(User.class)))
+                .thenAnswer(invocation -> invocation.getArgument(0));
+
+        // --- ACT ---
+        User result = userService.getOrCreateAndUpdateUser(UserTestData.DEFAULT_ID, UserTestData.DEFAULT_USERNAME,
+                UserTestData.DEFAULT_AVATAR);
+
+        // --- ASSERT ---
+        verify(userRepository, times(1)).save(userCaptor.capture());
+        assertThat(result.getId()).isEqualTo(UserTestData.DEFAULT_ID);
+        assertThat(userCaptor.getValue().getUsername()).isEqualTo(UserTestData.DEFAULT_USERNAME);
+
+    }
+
+    @Test
+    @DisplayName("It should just return the user without calling 'save' if the data is identical")
+    void getOrCreateAndUpdateUser_ExistingUser_NoChanges() {
+        // --- ARRANGE ---
+        User existingUser = UserTestData.createValidUser();
+        when(userRepository.findById(UserTestData.DEFAULT_ID)).thenReturn(Optional.of(existingUser));
+
+        // --- ACT ---
+        User result = userService.getOrCreateAndUpdateUser(UserTestData.DEFAULT_ID, UserTestData.DEFAULT_USERNAME,
+                UserTestData.DEFAULT_AVATAR);
+
+        // --- ASSERT ---
+        verify(userRepository, never()).save(any(User.class));
+        assertThat(result).isEqualTo(existingUser);
+    }
+
+    @Test
     @DisplayName("It should update and save the user if the name or photo changes in Discord")
     void getOrCreateAndUpdateUser_ExistingUser_WithChanges() {
         // --- ARRANGE ---
@@ -45,25 +80,6 @@ public class UserServiceTest {
         User savedUser = userCaptor.getValue();
         assertThat(savedUser.getUsername()).isEqualTo(newDiscordName);
         assertThat(result.getUsername()).isEqualTo(newDiscordName);
-    }
-
-    @Test
-    @DisplayName("Must create and save a new user if one does not exist in the database")
-    void getOrCreateAndUpdateUser_NewUser() {
-        // --- ARRANGE ---
-        when(userRepository.findById(UserTestData.DEFAULT_ID)).thenReturn(Optional.empty());
-        when(userRepository.save(any(User.class)))
-                .thenAnswer(invocation -> invocation.getArgument(0));
-
-        // --- ACT ---
-        User result = userService.getOrCreateAndUpdateUser(UserTestData.DEFAULT_ID, UserTestData.DEFAULT_USERNAME,
-                UserTestData.DEFAULT_AVATAR);
-
-        // --- ASSERT ---
-        verify(userRepository, times(1)).save(userCaptor.capture());
-        assertThat(result.getId()).isEqualTo(UserTestData.DEFAULT_ID);
-        assertThat(userCaptor.getValue().getUsername()).isEqualTo(UserTestData.DEFAULT_USERNAME);
-
     }
 
 }
