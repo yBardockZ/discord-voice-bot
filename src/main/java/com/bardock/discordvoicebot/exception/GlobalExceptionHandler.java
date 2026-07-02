e32eba400cd3b9f20231293ebcf6efd524d93b97
Thalles <thalles_leopoldino@outlook.com>
2026-07-02 05:38:57 -0300

    GlobalExceptionHandler implemented

---
+++ src/main/java/com/bardock/discordvoicebot/exception/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.bardock.discordvoicebot.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.Instant;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ProblemDetail handleResourceNotFoundException(ResourceNotFoundException ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
+        problemDetail.setTitle("Resource not found");
+        problemDetail.setProperty("timestamp", Instant.now());
+
+        return problemDetail;
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ProblemDetail handleGenericException(Exception ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, "An unexpected internal error has occurred.");
+        problemDetail.setTitle("Erro Interno do Servidor");
+        problemDetail.setProperty("timestamp", Instant.now());
+        // problemDetail.setProperty("cause", ex.getMessage());
+
+        return problemDetail;
+    }
+
+}
