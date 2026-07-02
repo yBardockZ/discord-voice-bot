777ef5b6d75153f798bb66225d78167a9f653907
Thalles <thalles_leopoldino@outlook.com>
2026-07-02 05:26:35 -0300

    RFC problem details enabled and ResourceNotFoundException created

---
+++ src/main/java/com/bardock/discordvoicebot/exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.bardock.discordvoicebot.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException (String message) {
+        super(message);
+    }
+
+}
