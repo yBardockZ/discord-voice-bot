53244bc42c6d847bacca0f78eb85f5a0ed3ef78b
Thalles <thalles_leopoldino@outlook.com>
2026-07-03 04:56:56 -0300

    handle MethodArgumentTypeMismatchException

--- src/main/java/com/bardock/discordvoicebot/exception/GlobalExceptionHandler.java
+++ src/main/java/com/bardock/discordvoicebot/exception/GlobalExceptionHandler.java
@@ -1,11 +1,15 @@
 package com.bardock.discordvoicebot.exception;
 
+import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import tools.jackson.databind.exc.MismatchedInputException;
 
+import java.net.URI;
 import java.time.Instant;
 
 @RestControllerAdvice
@@ -15,6 +19,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     public ProblemDetail handleResourceNotFoundException(ResourceNotFoundException ex) {
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
         problemDetail.setTitle("Resource not found");
+        problemDetail.setType(URI.create("about:blank"));
         problemDetail.setProperty("timestamp", Instant.now());
 
         return problemDetail;
@@ -23,9 +28,26 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ProblemDetail handleGenericException(Exception ex) {
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, "An unexpected internal error has occurred.");
-        problemDetail.setTitle("Erro Interno do Servidor");
+        problemDetail.setTitle("Internal server error");
+        problemDetail.setType(URI.create("about:blank"));
         problemDetail.setProperty("timestamp", Instant.now());
         // problemDetail.setProperty("cause", ex.getMessage());
+
+        return problemDetail;
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ProblemDetail handleTypeMismatch(MethodArgumentTypeMismatchException ex) {
+        String paramName = ex.getPropertyName();
+        String Expectedtype = ex.getRequiredType() != null ? ex.getRequiredType().getName() : "unknown";
+
+        String detailMessage = String.format("the parameter '%s' received an invalid value. " +
+                "Make sure you send an %s.", paramName, Expectedtype);
+
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, detailMessage);
+        problemDetail.setTitle("Invalid parameter in the URL");
+        problemDetail.setType(URI.create("about:blank"));
+        problemDetail.setProperty("timestamp", Instant.now());
 
         return problemDetail;
     }
