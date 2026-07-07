55ff66bbeeb369874e188ee1b439d42d83e8b1f2
Thalles <thalles_leopoldino@outlook.com>
2026-07-07 05:37:36 -0300

    Adding Springdoc Dependencies

--- src/main/java/com/bardock/discordvoicebot/exception/GlobalExceptionHandler.java
+++ src/main/java/com/bardock/discordvoicebot/exception/GlobalExceptionHandler.java
@@ -1,13 +1,11 @@
 package com.bardock.discordvoicebot.exception;
 
-import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import tools.jackson.databind.exc.MismatchedInputException;
 
 import java.net.URI;
 import java.time.Instant;
